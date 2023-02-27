7654cd3739aa6abf26892378a001589fd4f3bb06
Nico <588438+rybnico@users.noreply.github.com>
2023-02-27 23:53:34 +0100

    Revert "Error on `ptr` mechanisms"
    This reverts commit a880056217559559167ecee94a192575de4c9fa9.

--- octodns/processor/spf.py
+++ octodns/processor/spf.py
@@ -73,13 +73,10 @@ class SpfDnsLookupProcessor(BaseProcessor):
                     f"{record.fqdn} exceeds the 10 DNS lookup limit in the SPF record"
                 )
 
-            if term.startswith('ptr'):
-                raise SpfValueException(
-                    f"{record.fqdn} uses the deprecated ptr mechanism"
-                )
-
             # These mechanisms cost one DNS lookup each
-            if term.startswith(('a', 'mx', 'exists:', 'redirect', 'include:')):
+            if term.startswith(
+                ('a', 'mx', 'exists:', 'redirect', 'include:', 'ptr')
+            ):
                 lookups += 1
 
             # The include mechanism can result in further lookups after resolving the DNS record
